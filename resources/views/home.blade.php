19c8b4e51d8393b3ffaf23f585e6d463399082f0
Doris Morales <dmorales55@gmail.com>
2020-10-18 14:54:57 -0500

    Rutas basicas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.template')
+
+@section('main')
+  <h1>The Vinyl Shop</h1>
+  <p>
+    Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca.
+  </p>
+@endsection
