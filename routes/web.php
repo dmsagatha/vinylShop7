28c102bf87b0f99ecbba849e22ce97b916e8019a
Doris Morales <dmorales55@gmail.com>
2020-10-18 14:28:43 -0500

    Custom error pages

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('layouts.template');
 });
+
+Route::get('mySillyPage', function () {
+  // abort with error 404
+  //return abort('404');
+  // abort with error 403 (default error message)
+  // return abort('403');
+  // abort with error 403 (custom error message)
+  return abort('403', 'Mi tonto error');
+});
