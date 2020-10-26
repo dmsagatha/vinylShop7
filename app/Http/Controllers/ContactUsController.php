a406edd968bf9449ce9502d93383c08544a86d35
Doris Morales <dmorales55@gmail.com>
2020-10-26 15:35:21 -0500

    Carro de compras y el historial de ordenes del usuario

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -14,12 +14,12 @@ class ContactUsController extends Controller
     return view('contact');
   }
 
-  // Send email
+  // Enviar correo electrónco
   public function sendEmail(Request $request)
   {
-    // Validate form
+    // Validar formulario
 
-    // Send email
+    // Enviar correo electrónco
     $this->validate($request,[
       'name'    => 'required',
       'email'   => 'required|email',
@@ -28,18 +28,19 @@ class ContactUsController extends Controller
 
     // $email = new ContactMail();
     $email = new ContactMail($request);
-    // Descomente esta línea para mostrar el resultado en el navegador
+    // Descomentar esta línea para mostrar el resultado en el navegador
     // return $email; 
     Mail::to($request)      // o Mail::to($request->email, $request->name)
         ->send($email);
 
     // Flash de los valores del formulario rellenado a la sesión
     $request->flash();
+
     // Envía un mensaje de éxito a la sesión - session()->flash('key','value')
     session()->flash('success', 'Gracias por su mensaje.<br>Nos pondremos en contacto con usted lo mas pronto posible.');
     //return redirect('contact-us')->with('success', 'Thank you ...');
 
-    // Redirect to the contact-us link ( NOT to view('contact')!!! )
+    // Redireccionar al enlace de contact-us ( NO a view('contact')!!! )
     return redirect('contact-us');
   }
 }
