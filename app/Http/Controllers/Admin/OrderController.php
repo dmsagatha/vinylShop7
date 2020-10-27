9e9422fc31f481b03bb132a0d0c148ad60a6e82f
Doris Morales <dmorales55@gmail.com>
2020-10-26 20:39:57 -0500

    Uso avanzado de tablas vinculadas

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Json;
-use App\Models\Order;
+use App\Models\{Order, Orderline, User};
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -25,5 +25,48 @@ class OrderController extends Controller
     Json::dump($result);
 
     return view('admin.orders.index', $result);
+  }
+
+  //
+  /**
+   * Orderlines con order y user
+   * Recuperar toda la información de los pedidos con su línea de pedido 
+   * y el usuario al que pertenece este pedido
+   * Incluir todas las claves externas primarias y relevantes
+   */
+  public function orderlines()
+  {
+    //$orderlines = Orderline::with('order.user')->get();
+    //$orderlines = Orderline::with(['order', 'order.user'])->get();
+    $orderlines = Orderline::with(['order:id,user_id,total_price', 'order.user:id,name,email'])->get();
+
+    return $orderlines;
+  }
+  
+  /**
+   * User con orders y orderlines
+   * Recuperar toda la información de los pedidios del usuarios y los línea de
+   * pedidos
+   */
+  public function users()
+  {
+    //$users = User::with('orders.orderlines')->get();
+    //$users = User::with(['orders','orders.orderlines'])->get();
+    // $users = User::with(['orders:id,user_id,total_price', 'orders.orderlines:id,order_id,artist,title'])->get();
+
+    // Agregar condiciones de consulta adicionales
+    // Ordenar las líneas de orden (los registros que ordenó un usuario) por artista
+    $users = User::with(['orders' => function ($query) {
+      // Seleccionar los campos requeridos de la tabla orders
+      $query->select(['id', 'user_id', 'total_price'])
+          // Ir a la siguiente tabla (orderlines)
+          ->with(['orderlines' => function ($query) {
+              //  Seleccionar los campos requeridos de la tabla orderlines y ordenar por por artista
+              $query->select(['id', 'order_id', 'artist', 'title'])
+                  ->orderBy('artist');
+          }]);
+    }])->get();
+
+    return $users;
   }
 }
